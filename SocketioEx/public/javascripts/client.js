50c83d6d4116a0f8e965c2a0ad3aa67304e58ec2
hidetomo <hidetomo@www4322gi.sakura.ne.jp>
2014-08-14 15:57:51 +0900

    initial commit

---
+++ SocketioEx/public/javascripts/client.js
@@ -0,0 +1,20 @@
+jQuery(function($) {
+  "use strict";
+  //socket.ioのサーバにに接続
+  var socket = io.connect('http://'+location.host+'/');
+  
+  //サーバからmessageイベントが送信された時
+  socket.on('message',function(data){
+    $('#list').prepend($('<div/>').text(data.text));
+  });
+  //sendボタンがクリックされた時
+  $('#send').click(function(){
+    var text = $('#input').val();
+    if(text !== ''){
+      //サーバにテキストを送信
+      socket.emit('message',{text:text});
+      $('#list').prepend($('<div/>').text(text));
+      $('#input').val('');
+    }
+  });
+});
